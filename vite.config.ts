8f8d73e08a0b61ab3c71038057ebb49f2fee40ad
Your Name <you@example.comcante32@gmail.com>
2025-08-29 13:33:35 +0200

    Fix path resolution in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-import { fileURLToPath } from "url";
-
-// Get current directory path properly in ESM
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined
+    ...(process.env.NODE_ENV !== "production" &&
+    process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
             m.cartographer(),
